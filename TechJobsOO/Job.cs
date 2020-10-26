0f5f1c74adb645401b8490e55439bf5c3bd64009
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-10-26 18:53:35 -0400

    first three unit tests working except the testing of setting all fields

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace TechJobsOO
 {
     public class Job
--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace TechJobsTests
 {
     [TestClass]
-    public class UnitTest1
+    public class JobTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSettingJobId()
         {
+            Job job1 = new Job();
+            Job job2 = new Job();
+            Assert.IsFalse(job1.Id == job2.Id);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.AreEqual("Product tester", job1.Name);
+            Assert.AreEqual(new Employer("ACME"), job1.EmployerName);
+            Assert.AreEqual("Desert", job1.EmployerLocation);
+            Assert.AreEqual("Quality control", job1.JobType);
+            Assert.AreEqual("Persistence", job1.JobCoreCompetency);
+            //is there a better way to do all of these on one line?
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.IsFalse(job1.Equals(job2));
         }
     }
 }
